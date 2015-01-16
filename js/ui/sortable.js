889d17879d32a3522c093e6d88730752f2199f85
Toni Pinel <tonipinel@gmail.com>
2015-01-16 16:52:21 +0100

    New Sortable list type input

--- js/ui/sortable.js
+++ js/ui/sortable.js
@@ -2,56 +2,71 @@ TinyCore.AMD.define('sortable', ['devicePackage'], function () {
 	return {
 		onStart: function () {
 
-			var aTarget = document.querySelectorAll('[data-tc-modules="sortable"]'),
+			var aTargets = document.querySelectorAll('[data-tc-modules="sortable"]'),
 				self = this;
 
 			FC.trackEvent('JS_Libraries', 'call', 'sortable' );
 
 			require(['sortableLibs'], function() {
-				self.autobind(aTarget);
+				$(aTargets).each(function () {
+					self.autobind(this);
+				});
 			});
-
-
-
 		},
-		autobind: function (aTargets) {
+		autobind: function (oTarget) {
 
-			var self = this;
+			var oSettings = {};
 
-			$(aTargets).each(function () {
+			if (oTarget.getAttribute("data-tc-url") !== null) {
+				oSettings.ajax_url = oTarget.getAttribute("data-tc-url");
+			}
 
-				var oSettings = {},
-					oTarget = this;
+			if (oTarget.getAttribute("data-tc-input") !== null) {
+				oSettings.ajax_url = undefined;
+				oSettings.input =  oTarget.getAttribute("data-tc-input");
+			}
 
-				if (oTarget.getAttribute("data-tc-url") !== null) {
-					oSettings.ajax_url = this.getAttribute("data-tc-url");
-				}
+			if (oTarget.getAttribute("data-tc-object") !== null) {
+				oSettings.object = oTarget.getAttribute("data-tc-object");
+			} else {
+				oSettings.object = 'sortable';
+			}
 
-				if (oTarget.getAttribute("data-tc-object") !== null) {
-					oSettings.object = this.getAttribute("data-tc-object");
-				} else {
-					oSettings.object = 'sortable';
-				}
+			if (oTarget.getAttribute("data-tc-handle") !== null) {
+				oSettings.handle = oTarget.getAttribute("data-tc-handle");
+			}
 
-				if (oTarget.getAttribute("data-tc-handle") !== null) {
-					oSettings.handle = this.getAttribute("data-tc-handle");
-				}
+			$(oTarget).sortable(oSettings);
 
-				$(this).sortable(oSettings);
-
-				if (oSettings.ajax_url !== undefined && oSettings.ajax_url !== null) {
-					$(this).bind('sortupdate', function () {
-						$.ajax({
-							url: oSettings.ajax_url,
-							data: {
-								order: $(oTarget).serializeTree("id", oSettings.object)
-							},
-							type: 'POST',
-							dataType: 'json'
-						});
+			if (oSettings.ajax_url !== undefined && oSettings.ajax_url !== null) {
+				$(oTarget).bind('sortupdate', function () {
+					$.ajax({
+						url: oSettings.ajax_url,
+						data: {
+							order: $(oTarget).serializeTree("id", oSettings.object)
+						},
+						type: 'POST',
+						dataType: 'json'
 					});
-				}
-			});
+				});
+			} else {
+				$(oTarget).bind('sortupdate', function (index) {
+
+					var sValue = '',
+						nTotal = $('li', oTarget).length;
+
+					$('li', oTarget).each(function( nIndex ){
+
+						sValue += this.id;
+
+						if ( nIndex !== nTotal - 1) {
+							sValue += ',';
+						}
+					});
+
+					document.getElementById(oSettings.input).value = sValue;
+				});
+			}
 		}
 	};
 });
