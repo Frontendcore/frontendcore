d47c2f89effc03d978bf44d757c78ee6fc65d32a
Toni Pinel <tonipinel@gmail.com>
2015-01-20 09:58:58 +0100

    add gitpush task

--- Gruntfile.js
+++ Gruntfile.js
@@ -319,6 +319,29 @@ module.exports = function (grunt) {
 				]
 			}
 		},
+		gitpush: {
+			scss: {
+				options: {
+					cwd: "css/core/",
+					remote: "origin",
+					branch: "master"
+				}
+			},
+			js: {
+				options: {
+					cwd: "build/static/js/",
+					remote: "origin",
+					branch: "master"
+				}
+			},
+			workspace: {
+				options: {
+					cwd: "",
+					remote: "origin",
+					branch: "master"
+				}
+			}
+		},
 		gittag: {
 			scss: {
 				options: {
