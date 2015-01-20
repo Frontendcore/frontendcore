71025eb1217e2445588979d62fd31ecf6bceaf54
Toni Pinel <tonipinel@gmail.com>
2015-01-20 07:46:24 +0100

    Improved documentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,36 @@
 /*global module:false*/
 
-var pkg = require('./package.json');
+function mergeJSON(source1,source2){
+	/*
+	 * Properties from the Souce1 object will be copied to Source2 Object.
+	 * Note: This method will return a new merged object, Source1 and Source2 original values will not be replaced.
+	 * */
+	var mergedJSON = Object.create(source2);// Copying Source2 to a new Object
+
+	for (var attrname in source1) {
+		if(mergedJSON.hasOwnProperty(attrname)) {
+			if ( source1[attrname]!==null && source1[attrname].constructor===Object ) {
+				/*
+				 * Recursive call if the property is an object,
+				 * Iterate the object and set all properties of the inner object.
+				 */
+				mergedJSON[attrname] = mergeJSON(source1[attrname], mergedJSON[attrname]);
+			}
+
+		} else {//else copy the property from source1
+			mergedJSON[attrname] = source1[attrname];
+
+		}
+	}
+
+	return mergedJSON;
+}
+
+
+var pkg = require('./package.json'),
+	bbdd = require('./database.json'),
+	json = mergeJSON(pkg, bbdd);
+
 
 module.exports = function (grunt) {
 	grunt.initConfig({
@@ -233,7 +263,7 @@ module.exports = function (grunt) {
 			dist: {
 				files: [
 					{
-						data: 'database.json',
+						data: json,
 						expand: true,
 						cwd: 'twig/',
 						src: ['**/*.html.twig', '!**/_*.html.twig'],
@@ -242,12 +272,6 @@ module.exports = function (grunt) {
 					}
 				]
 			}
-		},
-		version: {
-			options: {
-				base: 'package.json'
-			},
-			files: ['bower.json','build/static/js/bower.json', 'css/core/bower.json']
 		},
 		version: {
 			options: {
@@ -292,29 +316,6 @@ module.exports = function (grunt) {
 						cwd: "./"
 					}
 				]
-			}
-		},
-		gitpush: {
-			scss: {
-				options: {
-					cwd: "css/core/",
-					remote: "origin",
-					branch: "master"
-				}
-			},
-			js: {
-				options: {
-					cwd: "build/static/js/",
-					remote: "origin",
-					branch: "master"
-				}
-			},
-			workspace: {
-				options: {
-					cwd: "",
-					remote: "origin",
-					branch: "master"
-				}
 			}
 		},
 		gittag: {
