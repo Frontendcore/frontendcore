b3d6565552c5f6939d1a6684008bd998fa86d3c7
Toni Pinel <tonipinel@gmail.com>
2015-01-29 05:56:54 +0100

    updated version and bug on grunt release

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function (grunt) {
     grunt.registerTask('js', ['newer:uglify', 'newer:jshint','newer:jasmine']);
 	grunt.registerTask('tests', ['newer:uglify','newer:jasmine']);
 	grunt.registerTask('twig', ['newer:twigRender']);
-	grunt.registerTask('release', ['version','default','gitadd:scss','gitadd:js','gitcommit:scss','gitcommit:js','gitadd:workspace','gitcommit:workspace','gittag','gitpush']);
+	grunt.registerTask('release', ['version','twigRender','scss','js','gitadd:scss','gitadd:js','gitcommit:scss','gitcommit:js','gitadd:workspace','gitcommit:workspace','gittag','gitpush']);
 	grunt.registerTask('stats', ['phantomas','gitadd:stats','gitcommit','gitpush:workspace']);
 	grunt.registerTask('scss', ['concurrent:compileSass', 'concurrent:documentSass','newer:cssmin','concurrent:templates']);
 	grunt.registerTask('log', ['clean:changelog','changelog','stencil']);
