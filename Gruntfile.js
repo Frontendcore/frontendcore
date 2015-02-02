fad20bbf9faf82e9bd17397628783729c532aebc
Toni Pinel <tonipinel@gmail.com>
2015-02-02 09:48:52 +0100

    Improved documentation of the JS architecture

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
 	grunt.registerTask('tests', ['newer:uglify','newer:jasmine']);
 	grunt.registerTask('build', ['grunt:clean_site','copy','default']);
 	grunt.registerTask('twig', ['newer:twigRender']);
-
+	grunt.registerTask('scss', ['concurrent:compileSass', 'grunt:clean_sassdoc','sassdoc','newer:cssmin','concurrent:templates']);
 	grunt.registerTask('commit', [
 		'version',
 		'grunt:clean_site',
@@ -41,7 +41,7 @@ module.exports = function (grunt) {
 	]);
 
 	grunt.registerTask('stats', ['phantomas','gitadd:stats','gitcommit','gitpush:workspace']);
-	grunt.registerTask('scss', ['concurrent:compileSass', 'concurrent:documentSass','newer:cssmin','concurrent:templates']);
+
 
 	grunt.registerTask('default', ['twig','scss', 'js']);
 
