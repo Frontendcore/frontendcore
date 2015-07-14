0c7f2fde9b8359a5d5933973d0273dbbdfda2368
Toni Pinel <tonipinel@gmail.com>
2015-07-14 08:21:06 +0200

    Bug Fixing on tracking for modules. Release version 1.6.11 !

--- js/base/_init.js
+++ js/base/_init.js
@@ -3,19 +3,19 @@ onDomReady(function () {
 	require(['devicePackage'], function() {
 		FrontendCore.domBoot(function (aModulesData) {
 			for (var nKey = 0; nKey < aModulesData.length; nKey++) {
-				FrontendTools.trackEvent('JS_Libraries', 'execute', aModulesData[nKey].name);
+				FrontendTools.trackModule('JS_Libraries', 'execute', aModulesData[nKey].name);
 			}
 		});
 	});
 
 	if ( oGlobalSettings.bResponsiveImages === true ) {
 		FrontendCore.requireAndStart('responsive-images');
-		FrontendTools.trackEvent('JS_Libraries', 'execute', 'responsive-images' );
+		FrontendTools.trackModule('JS_Libraries', 'execute', 'responsive-images' );
 	}
 
 	if ( oGlobalSettings.bCart === true ) {
 		FrontendCore.requireAndStart('cart');
-		FrontendTools.trackEvent('JS_Libraries', 'execute', 'cart' );
+		FrontendTools.trackModule('JS_Libraries', 'execute', 'cart' );
 	}
 
 });
