3fbbc3d8c85ff875433e6f539c232371c920c7a9
Toni Pinel <tonipinel@gmail.com>
2015-10-28 16:49:28 +0100

    Improved loadCSS to only load css mathing with the media query. Release Release version 1.7.19 !

--- js/tools/_loadCSS.js
+++ js/tools/_loadCSS.js
@@ -1,23 +1,48 @@
 FrontendTools.loadCSS = function ( sCssPath, sCssMedia ) {
 
-	var sId = sCssPath.replace('/','');
+	var sId = sCssPath.replace('/',''),
+		fMatchMedia = window.matchMedia,
+		sCssMedia = sCssMedia ? sCssMedia : 'all',
+		oCss = {};
+
+	function loadCss(sCssPath, sCssMedia) {
+
+		if ( sCssPath && !document.getElementById(sId) ) {
+			var head = document.getElementsByTagName('head')[0],
+				element = document.createElement('link');
+
+			element.rel = 'stylesheet';
+			element.type = 'text/css';
+			element.href = sCssPath;
+			element.id = sId;
+			element.media = 'non-existant-media';
+			head.appendChild(element, head.firstChild);
+			setTimeout(function () {
+				element.media = sCssMedia;
+			});
+		}
+	}
 
 	// If the CSS is not already loaded
-	if ( sCssPath && !document.getElementById(sId) ) {
+	if ( !fMatchMedia || window.matchMedia(sCssMedia).matches ) {
 
-		var head = document.getElementsByTagName('head')[0],
-			element = document.createElement('link');
+		loadCss(sCssPath, sCssMedia);
 
-		element.rel = 'stylesheet';
-		element.type = 'text/css';
-		element.href = sCssPath;
-		element.id = sId;
-		element.media = 'non-existant-media';
-		head.appendChild(element, head.firstChild);
-		setTimeout(function () {
-			element.media = sCssMedia ? sCssMedia : 'all';
-		});
+	} else if ( !window.matchMedia(sCssMedia).matches ) {
 
+		oCss[sCssPath] = sCssMedia;
+
+		window.addEventListener("resize", function() {
+
+			for ( var property in oCss) {
+
+				if  (window.matchMedia(oCss[property]).matches) {
+					loadCss( property, oCss[property] );
+				}
+			}
+		} );
 	}
+
+
 
 }
