4bbc73445fa5114194393aa3d9fb46bd03361d40
Toni Pinel <tonipinel@gmail.com>
2016-02-16 01:06:46 +0100

    Bug fixing on bind an navigation

--- components/essence/js/tools/_bind.js
+++ components/essence/js/tools/_bind.js
@@ -29,103 +29,10 @@ FrontendTools.bind = function ( selector , eventType, callback  ) {
             }
 
 
-            if ( oGlobalSettings.oBindElements[xPath].xpath === FrontendTools.getSelector(target)) {
+            if ( FrontendTools.getSelector(target).indexOf(oGlobalSettings.oBindElements[xPath].xpath) !== -1) {
                 callback.call(elms[i], event);
             }
         }
     });
 
-
-/*
-    var $Target = target instanceof jQuery ? target : $(target);
-
-    for ( var nKey = 0; nKey < $Target.length; nKey++  ) {
-
-        var oTarget = $Target[nKey],
-            sId = oTarget.id !== '' ? oTarget.id : $Target.selector;
-
-
-        if ( oTarget !== undefined ) {
-
-
-            if ( oGlobalSettings.oBindElements ===  undefined ) {
-                oGlobalSettings.oBindElements = {};
-            }
-
-            oGlobalSettings.oBindElements[sId] = oTarget;
-
-            if (sEvent === 'submit' ) {
-
-                var keyStop = {
-                    8: ":not(input:text, textarea, input:file, input:password)", // stop backspace = back
-                    13: "input:text, input:password", // stop enter = submit
-                    end: null
-                };
-
-                if ( oTarget.id === '') {
-                    oTarget.id = sId;
-                }
-
-                $(document).bind("keydown", function(e){
-
-                    var selector = keyStop[e.which],
-                        oParentForm = $(e.target).parents('form')[0];
-
-                    if( selector !== undefined && $(e.target).is(selector) ) {
-
-                        if ( oParentForm !== undefined ) {
-
-                            if ( oParentForm.id === oTarget.id ) {
-
-                                fpCallback(e);
-                            }
-                        }
-
-                    }
-
-                }).on('click' ,function(e) {
-
-                    var oParentForm = $(e.target).parents('form')[0];
-
-                    if ( e.target.nodeName === 'BUTTON' && oParentForm.id === oTarget.id ) {
-
-                        fpCallback(e);
-                    }
-                });
-
-            } else {
-
-                $(document).addEvent(sEvent ,function(e) {
-
-                    if (sEvent === 'click') {
-                        e.preventDefault();
-                    }
-
-                    var eventObject = $(e.target)[0],
-                        originalObject = $(oTarget)[0],
-                        currentObject;
-
-                    if ( eventObject.nodeName !== originalObject.nodeName ) {
-                        currentObject = eventObject.parentNode;
-                    } else {
-                        currentObject = eventObject;
-                    }
-
-                    if ( currentObject.dataset.fcModules !== undefined ) {
-                        var bModules = currentObject.dataset.fcModules == originalObject.dataset.fcModules;
-                    } else {
-                        var bModules = true;
-                    }
-
-                    if ( currentObject.nodeName == originalObject.nodeName && bModules && originalObject.className.indexOf(currentObject.className) !== -1  ) {
-                        fpCallback(e, currentObject);
-                    }
-
-                });
-            }
-
-        }
-
-    }
-*/
 };
