4d67ffedf5bd06e05261b3fc38d74dd70b8153ce
Toni Pinel <tonipinel@gmail.com>
2016-02-23 15:55:10 +0100

    JScompilation

--- grunt/copy.js
+++ grunt/copy.js
@@ -1,42 +1,66 @@
 module.exports = function(grunt) {
 
 	var oData = require(grunt.option('appCwd') + '/frontendcore.json'),
-		sModulesFolder = oData.js.modulesFolder !== undefined ? '/' + oData.js.modulesFolder : '/modules' ;
+		jsCwd = grunt.option('appCwd') +'/',
+		jsSrc = '**/*.js',
+		jsModulesFolder,
+		oConfig = {
+			Polyfills: {
+				files: [
+					{
+						cwd: grunt.option('fcCwd') +'/_resources/polyfills/',
+						expand: true,
+						flatten: false,
+						src: ['**/*.*'],
+						dest: grunt.option('fcCwd') +'/build/static/js/shims'
+					}
+				]
+			},
+			static: {
+				files: [
+					{
+						cwd: grunt.option('fcCwd') +'/_resources/static/',
+						expand: true,
+						flatten: false,
+						src: ['**/*.*'],
+						dest: grunt.option('fcCwd') +'/build/static/'
+					}
+				]
+			}
+		};
 
-	return {
-		Polyfills: {
-			files: [
-				{
-					cwd: grunt.option('fcCwd') +'/_resources/polyfills/',
-					expand: true,
-					flatten: false,
-					src: ['**/*.*'],
-					dest: grunt.option('fcCwd') +'/build/static/js/shims'
-				}
-			]
-		},
-		static: {
-			files: [
-				{
-					cwd: grunt.option('fcCwd') +'/_resources/static/',
-					expand: true,
-					flatten: false,
-					src: ['**/*.*'],
-					dest: grunt.option('fcCwd') +'/build/static/'
-				}
-			]
-		},
-		js: {
-			files: [
-				{
-					cwd: grunt.option('appCwd') +'/'+ oData.js.cwd + '/',
-					expand: true,
-					flatten: false,
-					src: ['**/*.js'],
-					dest: grunt.option('appCwd') +'/'+ oData.js.dest +'/' + sModulesFolder+ '/'
-				}
-			]
-		},
+
+	if ( grunt.option('project') ) {
+		oData = oData[grunt.option('project')];
 	}
+
+	if ( oData.js !== undefined) {
+
+		if (oData.js.cwd !== undefined) {
+			jsCwd += oData.js.cwd + '/';
+		}
+
+		jsModulesFolder =  grunt.option('appCwd') +'/'+ oData.js.dest +'/';
+
+		if (  oData.js.modulesFolder !== undefined ) {
+			jsModulesFolder += oData.js.modulesFolder + '/';
+		} else {
+			jsModulesFolder += 'modules/';
+		}
+	}
+
+	oConfig['js'] = {
+		files: [
+			{
+				cwd: jsCwd,
+				expand: true,
+				flatten: true,
+				src: [jsSrc],
+				dest:jsModulesFolder
+			}
+		]
+	};
+
+	return oConfig;
 
 }
