a12c16d55896b40e1ca78b2add69ab0226371be0
Toni Pinel <tonipinel@gmail.com>
2016-03-09 12:37:36 +0100

    css import taks

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,6 +26,7 @@ module.exports = function (grunt) {
 	grunt.registerTask('js', ['jshint','copy:js','copy:jsForms','uglify','notify:js']);
 	grunt.registerTask('js:compile', ['uglify:components','notify:js']);
 	grunt.registerTask('css', ['sass_globbing','css:compile']);
+	grunt.registerTask('css:import', ['sass_globbing']);
 	grunt.registerTask('css:one', ['sass_globbing','css:compile']);
 	grunt.registerTask('css:compile', ['sass','postcss','notify:css']);
 	grunt.registerTask('icons', ['webfont','notify:icons']);
