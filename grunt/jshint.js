5ee0e923d5ddd9f08f6db8944e7ab035bee6b015
Ton Pinel González <toni@plazah.com>
2017-03-13 13:03:45 +0100

    Joshing error on compile bug fixing

--- grunt/jshint.js
+++ grunt/jshint.js
@@ -4,37 +4,38 @@ module.exports = function(grunt) {
 
 	var aFiles = [
 			grunt.option('fcCwd') + 'Gruntfile.js',
-			grunt.option('fcCwd') + '/components/**/js/*.js',
-			'!' + grunt.option('fcCwd') + '/components/**/js/roundtrip.js',
-			'!' + grunt.option('fcCwd') + '/components/**/libs/**/*.js',
-			'!' + grunt.option('fcCwd') + '/components/**/dist/**/*.js',
+			grunt.option('fcCwd') + 'components/**/js/*.js',
+			'!' + grunt.option('fcCwd') + 'components/**/js/roundtrip.js',
+			'!' + grunt.option('fcCwd') + 'components/**/libs/**/*.js',
+			'!' + grunt.option('fcCwd') + 'components/**/dist/**/*.js',
 		],
 		oConfig = {
-			options: {
-				globals: {
-					console: true
-				}
+			all : {
+                options: {
+                    globals: {
+                        console: true
+                    }
+                }
 			}
 		};
 
 	if ( Object.prototype.toString.call(oData.js.cwd) === '[object Array]') {
 
 		for (var i = 0; i < oData.js.cwd.length; i++) {
-			aFiles.push(oData.js.cwd[i] + '/**/*.js');
+            aFiles.push(appCwd + '/' + oData.js.cwd[i] + '/**/*.js');
 		}
-
-        oConfig['files'] = {
-			src:  aFiles
-        };
 
 	}
 	else if ( jsCwd !== '') {
-
 		aFiles.push(jsCwd + '/**/*.js');
 
-		oConfig['files'] = aFiles;
 	}
 
-	return oConfig;
+    oConfig.all = {
+        src:  aFiles
+    };
+
+    return oConfig;
+
 
 }
